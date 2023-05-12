3943930e402084c18a18cc9773d7f2a49ba426fd
Chris Ian Fiel <ccfiel@gmail.com>
2023-05-12 11:08:45 +0800

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,40 +29,40 @@
 </script>
 
 {#if browser}
-<main>
-	<h1>Welcome to the <code>@uppy/svelte</code> demo!</h1>
-	<h2>Inline Dashboard</h2>
-	<label>
-		<input type="checkbox" bind:checked={showInlineDashboard} />
-		Show Dashboard
-	</label>
-	{#if showInlineDashboard}
-		<Dashboard uppy={uppy1} plugins={['Webcam']} />
-	{/if}
-	<h2>Modal Dashboard</h2>
-	<div>
-		<button on:click={() => (open = true)}>Show Dashboard</button>
-		<DashboardModal
-			uppy={uppy2}
-			{open}
+	<main>
+		<h1>Welcome to the <code>@uppy/svelte</code> demo!</h1>
+		<h2>Inline Dashboard</h2>
+		<label>
+			<input type="checkbox" bind:checked={showInlineDashboard} />
+			Show Dashboard
+		</label>
+		{#if showInlineDashboard}
+			<Dashboard uppy={uppy1} plugins={['Webcam']} />
+		{/if}
+		<h2>Modal Dashboard</h2>
+		<div>
+			<button on:click={() => (open = true)}>Show Dashboard</button>
+			<DashboardModal
+				uppy={uppy2}
+				{open}
+				props={{
+					onRequestCloseModal: () => (open = false),
+					plugins: ['Webcam']
+				}}
+			/>
+		</div>
+
+		<h2>Drag Drop Area</h2>
+		<DragDrop uppy={uppy1} />
+
+		<h2>Progress Bar</h2>
+		<ProgressBar
+			uppy={uppy1}
 			props={{
-				onRequestCloseModal: () => (open = false),
-				plugins: ['Webcam']
+				hideAfterFinish: false
 			}}
 		/>
-	</div>
-
-	<h2>Drag Drop Area</h2>
-	<DragDrop uppy={uppy1} />
-
-	<h2>Progress Bar</h2>
-	<ProgressBar
-		uppy={uppy1}
-		props={{
-			hideAfterFinish: false
-		}}
-	/>
-</main>
+	</main>
 {/if}
 
 <style global>
